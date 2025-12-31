f04d4b52eb85112d1e61dcb9b9142df0a046de36
Apsco-app <you@example.com>
2025-12-31 00:40:38 -0800

    STEM 07

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next"
+
 
 import Index from "./pages/Index";
 import MissionPage from "./pages/MissionPage";
@@ -51,7 +51,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <Analytics />
+
       {MAINTENANCE_MODE ? (
         <MaintenancePage />
       ) : (
